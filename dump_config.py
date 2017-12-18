2fafdaf05ec1006b8616d0c7acebf631c04bac0a
sacascio <sacascio@cisco.com>
2017-12-18 17:28:06 -0500

    working_finally
    finally

--- dump_config.py
+++ dump_config.py
@@ -19,7 +19,6 @@ def gettoken(ip,port,username,password):
     
     if response.status_code == 200:
         return response.json()['token']
-        print response.json()['token']
     
     else:
         print "Could not get token for authentication, error code %s" % (response.status_code)
@@ -27,20 +26,18 @@ def gettoken(ip,port,username,password):
     
    
 def getdata(ip,port,token,url):
-    
+    token = token.strip()
     requests.packages.urllib3.disable_warnings()
-    headers={ 'Content-Type':'application/json' }
+    headers={ 'Content-Type':'application/json', 'CustomToken' : token }
     #print "https://%s:%s%s" % (ip, port,url)
-    payload = { 'CustomToken' : token }
-    print payload
+
     response = requests.get("https://%s:%s%s" % (ip, port,url),
-                             data=json.dumps(payload),
                              headers=headers,
                              verify=False,                      # disable SSH certificate verification
                              timeout=30)
     
     if response.status_code == 200:
-        print response.json()
+        return response.json()
     
     else:
         print "Could not get %s, error code %s" % (url,response.status_code)
@@ -56,8 +53,16 @@ def main(argv):
     
     token = gettoken(ip,port,username,password)
 
-    getdata(ip,port,token,"/vsm/resources")
+    data = getdata(ip,port,token,"/vsm/resources")
+    print str(len(data)) + " record"
+    for d in data:
+        print d['ipAddress']
+        print d['swVersion']
+        print d['type']
+        print d['name']
 
+    data = getdata(ip,port,token,"/vsm/lineups")
+    print data
 
 if __name__ == '__main__':
     main(sys.argv[1:])
